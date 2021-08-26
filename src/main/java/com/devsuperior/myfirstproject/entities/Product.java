cd975a5cc3b594e9ab08fd53d564ee90817de996
Adriano Pequeno <adrian.pekeno@gmail.com>
2021-08-25 22:16:59 -0300

    Product memory

---
+++ src/main/java/com/devsuperior/myfirstproject/entities/Product.java
@@ -0,0 +1,84 @@
+package com.devsuperior.myfirstproject.entities;
+
+import java.io.Serializable;
+
+public class Product implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long Id;
+	private String name;
+	private Double price;
+	
+	private Category category;
+	
+	public Product() {
+	}
+
+	public Product(Long id, String name, Double price, Category category) {
+		super();
+		Id = id;
+		this.name = name;
+		this.price = price;
+		this.category = category;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+	
+	
+}
